9ad1721d800eb0bf0a324b6ddb876cebcaf0eaec
Abdulmohaimin Bashir <121438644+ValhallaAMB@users.noreply.github.com>
2025-09-11 17:10:41 +0300

    0009-PALINDROME-NUMBER Stats: Time: 14 ms (20.47%), Space: 17.8 MB (39.7%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,5 +1,6 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        # Cannot be a palindrome if 0 
         if (x < 0):
             return False
             
@@ -7,8 +8,9 @@ class Solution:
         copyX: int = x
 
         while x > 0:
+            # Save the resut in opposite order
             res = (res * 10) + (x % 10)
             x //= 10
-            print(res)
-            
+            # print(res)
+
         return copyX == res
