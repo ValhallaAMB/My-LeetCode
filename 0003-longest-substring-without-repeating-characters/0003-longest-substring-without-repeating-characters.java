bd4e87237a519840722620f924923afc38715998
Abdulmohaimin Bashir <121438644+ValhallaAMB@users.noreply.github.com>
2025-09-11 17:03:38 +0300

    0003-LONGEST-SUBSTRING-WITHOUT-REPEATING-CHARACTERS Stats: Time: 6 ms (72.28%), Space: 44.8 MB (58.09%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -3,17 +3,22 @@ class Solution {
         int maxLength = 0;
         int j = 0;
 
+        // Used HashSet because order is not necessary 
         HashSet<Character> hs = new HashSet<>();
 
         for (int i = 0; i < s.length(); i++) {
+            // Check if our HashSet contains the letter
             if (hs.contains(s.charAt(i))) {
+                // Repeat until the character has been removed
                 while (hs.contains(s.charAt(i))) {
                     hs.remove(s.charAt(j));
                     j++;
                 }
             }
 
+            // Add to HashSet
             hs.add(s.charAt(i));
+            // Calculate the max length
             maxLength = Math.max(maxLength, i - j + 1);
         }
 
