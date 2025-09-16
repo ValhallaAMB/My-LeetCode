217a8db2fbbc75ad4654f115cb6d4049036934fc
Abdulmohaimin Bashir <121438644+ValhallaAMB@users.noreply.github.com>
2025-09-17 01:39:36 +0300

    Added comments to solution

--- 1064-smallest-integer-divisible-by-k/1064-smallest-integer-divisible-by-k.py
+++ 1064-smallest-integer-divisible-by-k/1064-smallest-integer-divisible-by-k.py
@@ -1,12 +1,15 @@
 class Solution:
     def smallestRepunitDivByK(self, k: int) -> int:
+        # Cases that'll never work
         if k % 2 == 0 or k % 10 == 0 or k % 5 == 0:
             return -1
         
-        res: int = 1 % k
+        calculation: int = 1 % k
         counter: int = 1
-        while res != 0:
-            res = (res * 10 + 1) % k
-            counter+=1
+
+        # Perform calculation until the answer is found
+        while calculation != 0:
+            calculation = (calculation * 10 + 1) % k
+            counter += 1
 
         return counter
